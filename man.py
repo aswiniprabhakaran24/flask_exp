a887e85c16e9acfdccdd4c10050a0abcbd8bc4b6
aswini <aswiniprabhakaran24@gmail.com>
2024-05-27 16:04:20 +0530

    fetched data successfullu

--- man.py
+++ man.py
@@ -1,9 +1,10 @@
 import serial
 from flask import Flask, render_template, request
+from flask_cors import CORS 
 import time
 
 app = Flask(__name__)
-
+CORS(app)  
 def send_hex(hex_number, ser):
     hex_bytes = bytes.fromhex(hex_number)
     ser.write(hex_bytes)
@@ -14,25 +15,28 @@ def receive_hex(ser):
     hex_number = received_bytes.hex()
     return hex_number
 
+@app.route('/up')
+def index():
+    return "Javascript"
+       
 @app.route('/remo', methods=['GET','POST'])
 def send_data():
     if request.method == 'POST':
-        ser = serial.Serial('COM2',9600)
-        hex_number=request.form['hex_data'] 
+        ser = serial.Serial('COM2', 9600)
+        hex_number = request.form['hex_data'] 
         print(hex_number)
-        
         time.sleep(1)
         hex_to_send = hex_number
         send_hex(hex_to_send, ser)
         time.sleep(0.5)
         received_hex = receive_hex(ser)
-        print("Received:",(receive_hex))
+        print("Received:", received_hex)
         ser.close()
         return render_template('remo.html', received_hex=received_hex)
     return render_template('remo.html')
-    
+
 if __name__ == "__main__":
     try:
-        app.run(debug=True)
+        app.run()
     except KeyboardInterrupt:
         print("\nExiting...")
