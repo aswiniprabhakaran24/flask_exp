0b7466f85aec56e28cc41daf547b2ea662b8d94f
aswini <aswiniprabhakaran24@gmail.com>
2024-05-27 19:27:13 +0530

    error occured : <!doctype html>

--- man.py
+++ man.py
@@ -4,7 +4,8 @@ from flask_cors import CORS
 import time
 
 app = Flask(__name__)
-CORS(app)  
+CORS(app)
+
 def send_hex(hex_number, ser):
     hex_bytes = bytes.fromhex(hex_number)
     ser.write(hex_bytes)
@@ -15,15 +16,12 @@ def receive_hex(ser):
     hex_number = received_bytes.hex()
     return hex_number
 
-@app.route('/up')
+@app.route('/up', methods=['GET','POST'])
 def index():
-    return "Javascript"
-       
-@app.route('/remo', methods=['GET','POST'])
-def send_data():
+    received_hex=0
     if request.method == 'POST':
         ser = serial.Serial('COM2', 9600)
-        hex_number = request.form['hex_data'] 
+        hex_number = request.form['hex_data']
         print(hex_number)
         time.sleep(1)
         hex_to_send = hex_number
@@ -31,6 +29,22 @@ def send_data():
         time.sleep(0.5)
         received_hex = receive_hex(ser)
         print("Received:", received_hex)
+        ser.close()
+    return received_hex
+
+@app.route('/remo', methods=['GET','POST'])
+def send_data():
+    if request.method == 'POST':
+        ser = serial.Serial('COM2', 9600)
+        hex_number = request.form['hex_data']
+        print(hex_number)
+        time.sleep(1)
+        hex_to_send = hex_number
+        send_hex(hex_to_send, ser)
+        time.sleep(0.5)
+        received_hex = receive_hex(ser)
+        print("Received:", received_hex)
+        
         ser.close()
         return render_template('remo.html', received_hex=received_hex)
     return render_template('remo.html')
