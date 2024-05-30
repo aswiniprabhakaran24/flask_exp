be2f7adc010626667a7f17ec2708be804860a829
aswini <aswiniprabhakaran24@gmail.com>
2024-05-30 19:07:12 +0530

    all dropdown boxes were mapped

--- man.py
+++ man.py
@@ -6,6 +6,7 @@ import json
 
 app = Flask(__name__)
 CORS(app)
+parsed_data = {}
 
 def send_hex(hex_number, ser):
     hex_bytes = bytes.fromhex(hex_number)
@@ -17,7 +18,7 @@ def receive_hex(ser):
     return received_bytes
 
 def parse_received_data(received_bytes, hex_num):
-    parsed_data = {}
+   
     try:
         received_hex = received_bytes.hex()
         print("Received hex:", received_hex)
@@ -28,7 +29,15 @@ def parse_received_data(received_bytes, hex_num):
             parsed_data['Source Address'] = received_bytes[15]
             parsed_data['Standby Time'] = received_bytes[11]
             parsed_data['Transmitter Power'] = received_bytes[9]
-     
+        elif hex_num == "0102041109":
+            parsed_data['Combo First Key'] = received_bytes[7]
+            parsed_data['Combo Second Key'] = received_bytes[5]
+            parsed_data['Combo Secure'] = received_bytes[3]
+        elif hex_num == "0102040F08":
+            parsed_data['Low battery v'] = received_bytes[3]
+        else:
+            print("Some error occured")
+            
     except Exception as e:
         print("Error parsing received data:", e)
     return parsed_data
@@ -37,18 +46,24 @@ def parse_received_data(received_bytes, hex_num):
 def index():
     try:
         ser = serial.Serial('COM2', 9600) 
-        hex_number = '0102040605'
-        if 'hex_data' in request.args:
-            hex_number = request.args['hex_data']
-        print("Hex number:", hex_number)
-        time.sleep(1)
-        hex_to_send = hex_number
-        send_hex(hex_to_send, ser)
-        time.sleep(0.5)
-        received_hex = receive_hex(ser)
-        print("Received:", received_hex)
+        hex_number = '0102040605' 
         
-        parsed_data = parse_received_data(received_hex, hex_number)
+        hex_list = ["0102041109", "0102040F08", "0102040605", "010204000C", "010206014E4F4D"]
+        for hex_value in hex_list:
+            print( hex_value)
+        
+            if 'hex_data' in request.args:
+                hex_number = request.args['hex_data']
+            print("Hex number:", hex_number)
+            # time.sleep(1)
+            hex_to_send = hex_value
+            send_hex(hex_to_send, ser)
+            time.sleep(0.5)
+            received_hex = receive_hex(ser)
+            print("Received:", received_hex)
+        
+            parsed_data = parse_received_data(received_hex, hex_value)
+        # parsed_data = "test ok"
         ser.close()
         
         
